88759853226f35b576f2adf7a61c58d2b2ea2bbc
luirow050308 <Luisrrr>
2025-03-26 11:35:58 +0100

    RPG

--- src/Ware/WarenManager.java
+++ src/Ware/WarenManager.java
@@ -4,7 +4,8 @@ public class WarenManager
 {
     public static void main(String[] args)
     {
-        Ware milchbube = new Milchpackung(4.5, 30, 0, "2.4.2025", false, "Kuh");
+        Ware milchbube = new Milchpackung(12.5, 30, 0,
+                "2.4.2025 (Silksong Ankündigung)", false, "Kuh");
         milchbube.ausgeben();
     }
 }
